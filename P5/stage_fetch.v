0f7d96276d31e17452a9d7e18665a97c5c31cc1a
DDoSolitary <DDoSolitary@gmail.com>
2020-12-05 15:25:46 +0800

    P5: halt on pc out of range

--- P5/stage_fetch.v
+++ P5/stage_fetch.v
@@ -10,13 +10,20 @@ module stage_fetch #(parameter ADDR_WIDTH = 12, ADDR_OFFSET = 'h3000) (
 	localparam MEM_LOW = ADDR_OFFSET >> 2;
 	localparam MEM_HIGH = MEM_LOW + (1 << (ADDR_WIDTH - 2));
 	reg [31:0] mem [MEM_HIGH - 1:MEM_LOW];
+	reg halted;
 
-	assign instr = mem[pc[ADDR_WIDTH - 1:2] | MEM_LOW];
+	wire [31:0] data = mem[pc[ADDR_WIDTH - 1:2] | MEM_LOW];
+	wire should_halt = halted || !(next_pc >= 'h3000 && next_pc < 'h4000) || ^data === 1'bx;
+	assign instr = should_halt ? 0 : data;
 
 	always @(posedge clk) begin
 		if (reset) begin
 			pc <= ADDR_OFFSET;
+			halted <= 0;
 			$readmemh("code.txt", mem);
-		end else pc <= next_pc;
+		end else begin
+			pc <= next_pc;
+			halted <= should_halt;
+		end
 	end
 endmodule
